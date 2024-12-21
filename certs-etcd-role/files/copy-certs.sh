eed1e2e7dd9416cd1d64e5ffcbed5c6153543878
okhytren <khytrenoleh@gmail.com>
2024-12-21 13:48:16 +0200

    Move to Jinja2 templates, CRI-O and Ansible built-in capabilities

--- certs-etcd-role/files/copy-certs.sh
+++ certs-etcd-role/files/copy-certs.sh
@@ -1,5 +1,5 @@
 NODES=(<ETCD1_IP> <ETCD2_IP> <ETCD3_IP>)
 
 for node in ${NODES[@]}; do
-  scp certs/ca.pem certs/etcd.pem certs/etcd-key.pem jenkins@$node:
+  scp certs/ca.pem certs/etcd.pem certs/etcd-key.pem ${USER}@$node:
 done
--- etcd-role/files/etcd-binaries-downoald.sh
+++ etcd-role/files/etcd-binaries-downoald.sh
@@ -1,5 +1,5 @@
-ETCD_VER=v3.5.1
+ETCD_VER=v3.5.17
 wget -q --show-progress "https://github.com/etcd-io/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz"
-tar zxf etcd-v3.5.1-linux-amd64.tar.gz
-sudo mv etcd-v3.5.1-linux-amd64/etcd* /usr/local/bin/
+tar zxf "etcd-${ETCD_VER}-linux-amd64.tar.gz"
+sudo mv "etcd-${ETCD_VER}-linux-amd64/etcd*" /usr/local/bin/
 sudo rm -rf etcd*
